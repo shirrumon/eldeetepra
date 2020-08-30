d92d3f050de911eb1c0e752eefe9f00ad492ae4a
shirru <shirrumoni@gmail.com>
2020-08-30 22:31:52 +0200

    create news-app

--- news/views.py
+++ news/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
 
 def news(request):
-    return render(request, 'news/news.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'news/news.html', {'posts': posts})
+
